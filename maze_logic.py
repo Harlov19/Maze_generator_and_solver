3183ec6e038b02f746c884b35ecc108d08a5fe41
Misganaw Habtamu <misganawa956@gmail.com>
2026-05-08 15:11:31 +0300

    Challenge: Added  chance to create cycles in generation logic

--- maze_logic.py
+++ maze_logic.py
@@ -10,11 +10,30 @@ def init_maze():
 def step_generator(curr, R, C, northWall, eastWall, visited_gen, gen_stack):
     r, c = curr
     visited_gen[r][c] = True
+    
+   
     neighbors = []
     if r < R-1 and not visited_gen[r+1][c]: neighbors.append((r+1, c, 'N'))
     if r > 0 and not visited_gen[r-1][c]: neighbors.append((r-1, c, 'S'))
     if c < C-1 and not visited_gen[r][c+1]: neighbors.append((r, c+1, 'E'))
     if c > 0 and not visited_gen[r][c-1]: neighbors.append((r, c-1, 'W'))
+
+   
+    if random.random() < 0.05:
+        visited_neighbors = []
+        if r < R-1 and visited_gen[r+1][c]: visited_neighbors.append((r+1, c, 'N'))
+        if r > 0 and visited_gen[r-1][c]: visited_neighbors.append((r-1, c, 'S'))
+        if c < C-1 and visited_gen[r][c+1]: visited_neighbors.append((r, c+1, 'E'))
+        if c > 0 and visited_gen[r][c-1]: visited_neighbors.append((r, c-1, 'W'))
+        
+        if visited_neighbors:
+            _, _, direction = random.choice(visited_neighbors)
+           
+            if direction == 'N': northWall[r+1][c] = 0
+            if direction == 'S': northWall[r][c] = 0
+            if direction == 'E': eastWall[r][c+1] = 0
+            if direction == 'W': eastWall[r][c] = 0
+    
 
     if neighbors:
         next_r, next_c, direction = random.choice(neighbors)
@@ -27,7 +46,6 @@ def step_generator(curr, R, C, northWall, eastWall, visited_gen, gen_stack):
     elif gen_stack:
         return gen_stack.pop()
     return None
-
 def step_solver(curr, END_NODE, R, C, northWall, eastWall, visited_solve, solve_stack):
     r, c = curr
     visited_solve[r][c] = True
