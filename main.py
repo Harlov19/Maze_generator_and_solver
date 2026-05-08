ff38ecd24e4eca2ae032c7e399aa3cb21f09e711
Misganaw Habtamu <misganawa956@gmail.com>
2026-05-08 14:29:30 +0300

    Implemented DFS maze generation logic

--- main.py
+++ main.py
@@ -2,25 +2,57 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from maze_logic import init_maze
-from maze_graphics import draw_maze
+from maze_logic import init_maze, step_generator 
+from maze_graphics import draw_maze, draw_marker 
+import random
 
+# Constants
 R, C = 20, 20
 CELL_SIZE = 2.0
 OFFSET_X, OFFSET_Y = -(C*CELL_SIZE)/2, -(R*CELL_SIZE)/2
+ANIMATION_SPEED = 30 # speed for generation
+
+# Data Structures
 northWall, eastWall = init_maze()
+visited_gen = [[False for _ in range(C)] for _ in range(R)]
+gen_stack = []
 
 def main():
     pygame.init()
     pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+    pygame.display.set_caption("Maze Generating...")
+    
     gluPerspective(45, 1.33, 0.1, 100.0)
     glTranslatef(0.0, 0.0, -60)
     
+    # Initial random starting point for the mouse
+    curr_gen = (random.randint(0, R-1), random.randint(0, C-1))
+    
+    clock = pygame.time.Clock()
+    generating = True
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: pygame.quit(); return
+        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        
+        # Draw the walls
         draw_maze(R, C, northWall, eastWall, OFFSET_X, OFFSET_Y, CELL_SIZE)
-        pygame.display.flip()
+        
+        # Move the generation mouse one step
+        if generating:
+            next_step = step_generator(curr_gen, R, C, northWall, eastWall, visited_gen, gen_stack)
+            if next_step:
+                curr_gen = next_step
+                
+                draw_marker(curr_gen[0], curr_gen[1], (1, 1, 1), OFFSET_X, OFFSET_Y, CELL_SIZE, 10)
+            else:
+                generating = False
+                pygame.display.set_caption("Maze Generation Complete!")
 
-if __name__ == '__main__': main()
+        pygame.display.flip()
+        clock.tick(ANIMATION_SPEED)
+
+if __name__ == '__main__':
+    main()
--- maze_logic.py
+++ maze_logic.py
@@ -1,7 +1,29 @@
 import random
 R, C = 20, 20
 
+# Returns the two walls
 def init_maze():
     northWall = [[1 for _ in range(C)] for _ in range(R + 1)] 
     eastWall = [[1 for _ in range(C + 1)] for _ in range(R)] 
     return northWall, eastWall
+
+def step_generator(curr, R, C, northWall, eastWall, visited_gen, gen_stack):
+    r, c = curr
+    visited_gen[r][c] = True
+    neighbors = []
+    if r < R-1 and not visited_gen[r+1][c]: neighbors.append((r+1, c, 'N'))
+    if r > 0 and not visited_gen[r-1][c]: neighbors.append((r-1, c, 'S'))
+    if c < C-1 and not visited_gen[r][c+1]: neighbors.append((r, c+1, 'E'))
+    if c > 0 and not visited_gen[r][c-1]: neighbors.append((r, c-1, 'W'))
+
+    if neighbors:
+        next_r, next_c, direction = random.choice(neighbors)
+        gen_stack.append((r, c))
+        if direction == 'N': northWall[r+1][c] = 0
+        if direction == 'S': northWall[r][c] = 0
+        if direction == 'E': eastWall[r][c+1] = 0
+        if direction == 'W': eastWall[r][c] = 0
+        return (next_r, next_c)
+    elif gen_stack:
+        return gen_stack.pop()
+    return None
